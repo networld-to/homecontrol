e1b59dfe37e7b3e565597283565a79c804efe001
Alex Oberhauser <alex.oberhauser@networld.to>
2018-02-25 19:18:51 -0500

    Removing unnecessary build.sh file. Makefile is available.

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-export PROJ="github.com/networld-to/homecontrol"
-go build -o build/homecontrol_server \
-  -ldflags "-s -w -X '${PROJ}/version.Version=$(git describe --always)'\
-  -X '${PROJ}/version.Build=$(date -u '+%Y-%m-%dT%H:%M:%SZ')'" server/main.go
-
-go build -o build/homecontrol_client \
-  -ldflags "-s -w -X '${PROJ}/version.Version=$(git describe --always)'\
-  -X '${PROJ}/version.Build=$(date -u '+%Y-%m-%dT%H:%M:%SZ')'" client/main.go
